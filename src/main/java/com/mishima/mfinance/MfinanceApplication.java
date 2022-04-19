3e73f15372c95ce8041bf0d80a744688448105c3
Mishima <sedovmikh@yandex.ru>
2022-04-19 10:06:15 +0300

    initial commit

---
+++ src/main/java/com/mishima/mfinance/MfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.mishima.mfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfinanceApplication.class, args);
+	}
+
+}
