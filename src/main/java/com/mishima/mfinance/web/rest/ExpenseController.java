f4764384a88a018439fb2eaf4c868e7bf1e35314
msedov <msedov@fil-it.ru>
2022-05-08 15:30:43 +0300

    expenses table styles

--- src/main/java/com/mishima/mfinance/web/rest/ExpenseController.java
+++ src/main/java/com/mishima/mfinance/web/rest/ExpenseController.java
@@ -1,6 +1,5 @@
 package com.mishima.mfinance.web.rest;
 
-import com.mishima.mfinance.entity.Expense;
 import com.mishima.mfinance.service.ExpenseService;
 import com.mishima.mfinance.service.dto.ExpenseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/expense")
@@ -26,9 +24,9 @@ public class ExpenseController {
     }
 
     @GetMapping("/{id}")
-    public String findById(@PathVariable Long id, Model model) {
-        Optional<ExpenseDTO> expenseOptional = expenseService.findById(id);
-        expenseOptional.ifPresent(exp -> model.addAttribute("expense", exp));
-        return "expenses";
+    @ResponseBody
+    public ExpenseDTO getExpenseById(@PathVariable Long id) {
+        return expenseService.findById(id).orElse(null);
     }
+
 }
